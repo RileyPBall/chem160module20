7c40eba5e180743047d53444a8ccba4f4bc8aecd
rileypball <rball4@ucmerced.edu>
2019-12-15 18:47:00 -0800

    module 20 commit

---
+++ count7.bash
@@ -0,0 +1,4 @@
+#!bin/bash
+for i in {7,14,21,28,35,42,49,56,63,70,77,84,91,98,105,112,119,126,133,140}; do
+	echo "Next multiple of 7 is $i"
+done
---
+++ echo.bash
@@ -0,0 +1,4 @@
+#!bin/bash
+echo "Script $0 has $# arguements"
+echo "The first 3 are $1, $2, $3"
+echo "All of the arguements are $*"
